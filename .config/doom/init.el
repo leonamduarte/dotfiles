27447a2f5a436dd4fcdc367b8d2f1b2b2f531802
leonamsh <llpdmonteiro@proton.me>
2025-11-26 13:37:04 -0300

    new emacs config
    Former-commit-id: 87285e184911aa2b01052b24bf4f52fd89b065a6

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -105,7 +105,7 @@
        ;;taskrunner        ; taskrunner for all your projects
        ;;terraform         ; infrastructure as code
        ;;tmux              ; an API for interacting with tmux
-       (tree-sitter +lsp)       ; syntax and parsing, sitting in a tree...
+       tree-sitter       ; syntax and parsing, sitting in a tree...
        ;;upload            ; map local to remote projects via ssh/ftp
 
        :os
@@ -190,6 +190,6 @@
        ;;twitter           ; twitter client https://twitter.com/vnought
 
        :config
-       (literate +tangle)
+       ;; (literate +tangle)
        (default +bindings +smartparens))
 
