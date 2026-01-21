421f972fa608cf2f4a2b835f08adb30bb05d03c5
leonamsh <llpdmonteiro@proton.me>
2026-01-21 11:28:28 -0300

    fix kanagawa theme
    Former-commit-id: 60eb884b00e8e5e602c846569a0a3000afa8cf8c

--- .config/nvim/lua/plugins/colorscheme/kanagawa.lua
+++ .config/nvim/lua/plugins/colorscheme/kanagawa.lua
@@ -7,7 +7,7 @@ return {
     require("kanagawa").setup({
       compile = true, -- compila em bytecode (startup mais rápido)
       undercurl = true,
-      transparent = true, -- mude pra true se usar terminal com blur
+      transparent = false, -- mude pra true se usar terminal com blur
       dimInactive = true,
 
       commentStyle = { italic = true },
@@ -37,30 +37,55 @@ return {
 
       overrides = function(colors)
         local theme = colors.theme
+        local makeDiagnosticColor = function(color)
+          local c = require("kanagawa.lib.color")
+          return { fg = color, bg = c(color):blend(theme.ui.bg, 0.95):to_hex() }
+        end
 
         return {
+          -- Assign a static color to strings
+          String = { fg = colors.palette.carpYellow, italic = true },
+          -- theme colors will update dynamically when you change theme!
+          SomePluginHl = { fg = colors.theme.syn.type, bold = true },
+
           -- Flutuantes limpos e modernos
           NormalFloat = { bg = "none" },
-          FloatBorder = { fg = theme.ui.bg_p2, bg = "none" },
+          FloatBorder = { bg = "none" },
+          FloatTitle = { bg = "none" },
+
+          -- Save an hlgroup with dark background and dimmed foreground
+          -- so that you can use it where your still want darker windows.
+          -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
+          NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+
+          -- Popular plugins that open floats will link to NormalFloat by default;
+          -- set their background accordingly if you wish to keep them dark and borderless
+          LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+          MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
 
           -- Linha atual mais sutil
           CursorLine = { bg = theme.ui.bg_p1 },
 
           -- Diagnósticos menos agressivos
-          DiagnosticVirtualTextHint = { fg = theme.diag.hint },
-          DiagnosticVirtualTextInfo = { fg = theme.diag.info },
-          DiagnosticVirtualTextWarn = { fg = theme.diag.warning },
-          DiagnosticVirtualTextError = { fg = theme.diag.error },
+          DiagnosticVirtualTextHint = makeDiagnosticColor(theme.diag.hint),
+          DiagnosticVirtualTextInfo = makeDiagnosticColor(theme.diag.info),
+          DiagnosticVirtualTextWarn = makeDiagnosticColor(theme.diag.warning),
+          DiagnosticVirtualTextError = makeDiagnosticColor(theme.diag.error),
 
           -- Popup de autocomplete mais elegante
-          Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },
-          PmenuSel = { bg = theme.ui.bg_p2 },
+          Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
+          PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+          PmenuSbar = { bg = theme.ui.bg_m1 },
           PmenuThumb = { bg = theme.ui.bg_p2 },
 
           -- Telescope estilo “bloco”
+          TelescopeTitle = { fg = theme.ui.special, bold = true },
+          TelescopePromptNormal = { bg = theme.ui.bg_p1 },
           TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+          TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
           TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
-          TelescopePreviewBorder = { fg = theme.ui.bg_dim, bg = theme.ui.bg_dim },
+          TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+          TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
         }
       end,
     })
