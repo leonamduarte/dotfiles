f834a124171f4d30697ecfecf0f088abd76aca08
strixy <lpdmonteiro@gmail.com>
2026-01-22 18:16:09 -0300

    minor updates
    Former-commit-id: 9d7515a6a54167a9b9c298d8c4b2f329a7ba3b58

--- .config/nvim/lua/plugins/orgmode.lua
+++ .config/nvim/lua/plugins/orgmode.lua
@@ -1,7 +1,4 @@
 -- ~/.config/nvim/lua/plugins/orgmode.lua
--- Configuração minimalista seguindo Unix Philosophy e "write simple software"
--- Reproduz Doom Emacs +org +dragndrop +journal +pandoc +pretty +roam
-
 return {
   -- Plugin principal: nvim-orgmode
   {
@@ -9,10 +6,8 @@ return {
     event = "VeryLazy",
     ft = { "org" },
     dependencies = {
-      "nvim-orgmode/org-bullets.nvim", -- +pretty: bullets bonitos
-      "chipsenkbeil/org-roam.nvim", -- +roam: zettelkasten
-      "nvim-telescope/telescope.nvim", -- busca e navegação
-      "nvim-orgmode/telescope-orgmode.nvim", -- integração telescope
+      "nvim-orgmode/org-bullets.nvim", -- Visual
+      "chipsenkbeil/org-roam.nvim", -- Zettelkasten
     },
     config = function()
       -- Setup básico do orgmode
@@ -20,36 +15,19 @@ return {
         org_agenda_files = "~/org/**/*",
         org_default_notes_file = "~/org/refile.org",
 
-        -- Templates de captura (+journal incluso)
+        -- Templates de captura
         org_capture_templates = {
-          -- Template padrão
-          t = {
-            description = "Task",
-            template = "* TODO %?\n  %u",
-          },
-          -- +journal: Daily journal
-          j = {
-            description = "Journal",
-            template = "* %<%H:%M> %?\n",
-            target = "~/org/journal/%<%Y-%m-%d>.org",
-          },
-          -- Quick note
-          n = {
-            description = "Note",
-            template = "* %?\n  %u",
-            target = "~/org/notes.org",
-          },
+          t = { description = "Task", template = "* TODO %?\n  %u" },
+          j = { description = "Journal", template = "* %<%H:%M> %?\n", target = "~/org/journal/%<%Y-%m-%d>.org" },
+          n = { description = "Note", template = "* %?\n  %u", target = "~/org/notes.org" },
         },
 
-        -- Configurações visuais simples
+        -- Configurações visuais
         org_hide_leading_stars = true,
         org_hide_emphasis_markers = true,
-
-        -- Exportação (+pandoc será via comando externo)
-        -- Org já suporta export nativo para HTML, LaTeX, etc
       })
 
-      -- +pretty: org-bullets para visual agradável
+      -- Visual: Bullets
       require("org-bullets").setup({
         symbols = {
           headlines = { "◉", "○", "✸", "✿" },
@@ -61,10 +39,9 @@ return {
         },
       })
 
-      -- +roam: org-roam para zettelkasten
+      -- Roam: Zettelkasten
       require("org-roam").setup({
         directory = "~/org/roam",
-        -- Dailies integrado (+journal funcionalidade)
         extensions = {
           dailies = {
             bindings = {
@@ -73,34 +50,24 @@ return {
               goto_yesterday = "<LocalLeader>ny",
             },
             templates = {
-              d = {
-                description = "daily",
-                template = "* %<%H:%M> %?",
-                target = "%<%Y-%m-%d>.org",
-              },
+              d = { description = "daily", template = "* %<%H:%M> %?", target = "%<%Y-%m-%d>.org" },
             },
           },
         },
       })
-
-      -- Telescope integração
-      require("telescope").load_extension("orgmode")
+      -- Removido: require("telescope").load_extension("orgmode")
     end,
   },
 
-  -- Plugin auxiliar: drag and drop de imagens (+dragndrop)
+  -- Plugin auxiliar: Drag & Drop de imagens
   {
     "HakonHarnes/img-clip.nvim",
     event = "VeryLazy",
     ft = { "org" },
     opts = {
-      default = {
-        dir_path = "~/org/images",
-      },
+      default = { dir_path = "~/org/images" },
       filetypes = {
-        org = {
-          template = "#+ATTR_ORG: :width 500\n[[file:$FILE_PATH]]",
-        },
+        org = { template = "#+ATTR_ORG: :width 500\n[[file:$FILE_PATH]]" },
       },
     },
     keys = {
@@ -108,23 +75,49 @@ return {
     },
   },
 
-  -- Keymaps adicionais em orgmode
+  -- Keymaps: Substituindo Telescope por FzfLua + Native Lua
   {
     "nvim-orgmode/orgmode",
     keys = function()
       return {
-        -- Org-roam keys
-        { "<leader>nf", "<cmd>lua require('org-roam').node.find()<cr>", desc = "Roam Find Node" },
+        -- --- Org-Roam ---
+        -- Como o org-roam usa internamente lógica de seleção, podemos usar FzfLua
+        -- para encontrar os arquivos físicos diretamente.
+        {
+          "<leader>nf",
+          function()
+            require("fzf-lua").files({ cwd = "~/org/roam", prompt = "Roam Nodes> " })
+          end,
+          desc = "Find Roam Node (Files)",
+        },
         { "<leader>ni", "<cmd>lua require('org-roam').node.insert()<cr>", desc = "Roam Insert Node" },
         { "<leader>nc", "<cmd>lua require('org-roam').capture.capture()<cr>", desc = "Roam Capture" },
 
-        -- Telescope orgmode
-        { "<leader>or", "<cmd>Telescope orgmode refile_heading<cr>", desc = "Refile Heading" },
-        { "<leader>oh", "<cmd>Telescope orgmode search_headings<cr>", desc = "Search Headings" },
-        { "<leader>ol", "<cmd>Telescope orgmode insert_link<cr>", desc = "Insert Link" },
+        -- --- Orgmode Navigation (FzfLua) ---
+        -- Busca textual em todos os arquivos org (substitui search_headings)
+        {
+          "<leader>oh",
+          function()
+            require("fzf-lua").live_grep({ cwd = "~/org", glob = "*.org", prompt = "Org Grep> " })
+          end,
+          desc = "Search Org Files (Grep)",
+        },
+        -- Busca arquivos org gerais
+        {
+          "<leader>of",
+          function()
+            require("fzf-lua").files({ cwd = "~/org", prompt = "Org Files> " })
+          end,
+          desc = "Find Org Files",
+        },
 
-        -- Export (+pandoc via comando shell)
-        -- Usa pandoc externamente, simples e Unix-way
+        -- --- Ações Nativas do Orgmode ---
+        -- O orgmode moderno usa vim.ui.select, então o FzfLua deve capturar isso automaticamente
+        -- se o LazyVim estiver com 'stevearc/dressing.nvim' ou 'fzf-lua' configurado como ui.select default.
+        { "<leader>or", "<cmd>OrgRefile<cr>", desc = "Refile Heading" },
+        { "<leader>ol", "<cmd>OrgInsertLink<cr>", desc = "Insert Link" },
+
+        -- --- Export (Pandoc) ---
         { "<leader>ep", ":!pandoc % -o %:r.pdf<CR>", desc = "Export to PDF", ft = "org" },
         { "<leader>eh", ":!pandoc % -o %:r.html<CR>", desc = "Export to HTML", ft = "org" },
         { "<leader>ed", ":!pandoc % -o %:r.docx<CR>", desc = "Export to DOCX", ft = "org" },
