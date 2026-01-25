f0b1e7c5cc06ffbfbb526059af9c43a06e91d562
strixy <lpdmonteiro@gmail.com>
2026-01-25 20:17:00 -0300

    novo pomodoro para fish shell

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -105,7 +105,7 @@ if status is-interactive
 
     # --- Sistema (PACMAN / ARCH) ---
     # Anatomia: -S (Sync), -y (Refresh DB), -u (Sys Upgrade)
-    # Importante: No Arch, evitamos o flag '--noconfirm' em aliases de update 
+    # Importante: No Arch, evitamos o flag '--noconfirm' em aliases de update
     # para garantir que você leia a lista de pacotes antes de quebrar algo.
 
     abbr --add update 'sudo pacman -Syu'
@@ -168,4 +168,31 @@ if status is-interactive
             --prompt='files> '
     end
 
+end
+
+function pom
+    set split $POMO_SPLIT
+    if ! test -n "$split"
+        set split $(gum choose "25/5" "50/10" "all done" --header "Choose a pomodoro split.")
+    end
+
+    switch $split
+        case 25/5
+            set work 25m
+            set break 5m
+        case 50/10
+            set work 50m
+            set break 10m
+        case 'all done'
+            return
+    end
+
+    timer $work && terminal-notifier -message Pomodoro \
+        -title 'Work Timer is up! Take a Break 😊' \
+        -sound Crystal
+
+    gum confirm "Ready for a break?" && timer $break && terminal-notifier -message Pomodoro \
+            -title 'Break is over! Get back to work 😬' \
+        -sound Crystal \
+        || pom
 end
