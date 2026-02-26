9d661c22a4f2108a7d2b6719b161d275c7d953b8
bashln <lpdmonteiro@gmail.com>
2026-02-25 22:00:57 -0300

    niri + dms configs

--- config/.config/nvim/lua/plugins/colorscheme/doomone.lua
+++ config/.config/nvim/lua/plugins/colorscheme/doomone.lua
@@ -4,7 +4,7 @@ return {
     lazy = false,
     priority = 1000,
     opts = {
-      transparent = false,
+      transparent = true,
       colors = {},
       highlights = {},
     },
--- stow.sh
+++ stow.sh
@@ -1,1 +1,1 @@
-stow -v -t ~ shell git config
+stow -v -t ~ shell git config --adopt
