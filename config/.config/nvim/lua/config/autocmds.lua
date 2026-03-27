0691df1f3c7b88f00d3cb63d5a639b5be29839ca
bashln <bashln@local>
2026-03-27 12:50:24 -0300

    fix(nvim): add explicit format on save autocmd using conform

--- config/.config/nvim/lua/config/autocmds.lua
+++ config/.config/nvim/lua/config/autocmds.lua
@@ -60,3 +60,15 @@ vim.api.nvim_create_autocmd("VimResized", {
   group = augroup("resize_splits"),
   command = "tabdo wincmd =",
 })
+
+-- Format on save using conform
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group = augroup("format_on_save"),
+  callback = function(args)
+    local conform = require("conform")
+    local formatters = conform.list_formatters(args.buf)
+    if formatters and #formatters > 0 then
+      conform.format({ bufnr = args.buf, timeout_ms = 2000 })
+    end
+  end,
+})
