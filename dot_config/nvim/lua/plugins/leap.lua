6213ec34fd4b1d8de7353f15f1e5016027d4269d
paesmont <paesmont@email.com>
2026-04-06 19:37:17 -0300

    nvim: leap.nvim — update upstream to codeberg (andyg/leap.nvim)

--- dot_config/nvim/lua/plugins/leap.lua
+++ dot_config/nvim/lua/plugins/leap.lua
@@ -1,5 +1,6 @@
 return {
-  "ggandor/leap.nvim",
+  -- upstream renamed from ggandor/leap.nvim to andyg/leap.nvim on Codeberg
+  "https://codeberg.org/andyg/leap.nvim.git",
   event = "VeryLazy",
   opts = {},
 }
