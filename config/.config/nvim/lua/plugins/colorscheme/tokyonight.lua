8730091e30fd41d4c70a209124d81fde96b50a70
leonamduarte <lpdmonteiro@gmail.com>
2026-04-14 20:36:03 -0300

    refactor: remove fish key bindings and enable tokyonight transparent

--- config/.config/nvim/lua/plugins/colorscheme/tokyonight.lua
+++ config/.config/nvim/lua/plugins/colorscheme/tokyonight.lua
@@ -4,6 +4,6 @@ return {
   priority = 1000,
   opts = {
     style = "storm", -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
-    transparent = false, -- Enable this to disable setting the background color
+    transparent = true, -- Enable this to disable setting the background color
   },
 }
